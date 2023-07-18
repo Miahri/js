4acf760217b01ff78ae1d8b16767412c227a716a
Miahri <mahrihommadova@gmail.com>
2023-07-18 16:11:16 +0300

    kata from codewars 5kyu first non repeating letter

--- src/codewars.js
+++ src/codewars.js
@@ -28,3 +28,36 @@ function isMerge(s, part1, part2) {
 }
 
 isMerge('codewars', 'cdwr', 'oeas');
+
+/*Write a function named first_non_repeating_letter that takes a string input, and returns the first character that is not repeated anywhere in the string.
+For example, if given the input 'stress', the function should return 't', since the letter t only occurs once in the string, and occurs first in the string.
+As an added challenge, upper- and lowercase letters are considered the same character, but the function should return the correct case for the initial letter. For example, the input 'sTreSS' should return 'T'.
+If a string contains all repeating characters, it should return an empty string ("") or None -- see sample tests.*/
+function firstNonRepeatingLetter(s) {
+    // Add your code here
+    let charsArr = [...s.toUpperCase()];
+    let count = {},
+        res;
+    for (let i = 0; i < charsArr.length; i++) {
+        if(count[charsArr[i]]) {
+            count[charsArr[i]] += 1;
+        } else {
+            count[charsArr[i]] = 1;
+        }
+    }
+    for (let key in count) {
+        if(count[key] === 1) {
+            res = key;
+            break;
+        } else res = "";
+    }
+    for (const letter of s) {
+        if(letter.toUpperCase() == res.toUpperCase() ) {
+            res = letter;
+        }
+    }
+
+    return res;
+}
+
+firstNonRepeatingLetter('sTreSS')
