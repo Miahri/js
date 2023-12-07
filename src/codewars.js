7753dc337dffbd97484c2be7c735564d31025420
Miahri <mahrihommadova@gmail.com>
2023-12-08 02:20:28 +0300

    kata from codewars 5kyu //[1,2,3,4,5,6,7], 3 => [3,6,2,7,5,1,4]

--- src/codewars.js
+++ src/codewars.js
@@ -2,3 +2,19 @@
 const pigIt = s => s.replace(/(\w)(\w*)/g, '\$2\$1ay');
 
 pigIt('Hello World!');
+
+//[1,2,3,4,5,6,7], 3 => [3,6,2,7,5,1,4]
+function josephus(items,k){
+    //your code here
+    let p = 0,
+        res = [];
+    if (items.length > 1 && k !== 1) {
+        while (items.length > 0) {
+            p = (p + k - 1) % items.length;
+            let splicedItem = items.splice(p, 1);
+            res.push(...splicedItem);
+        }
+    } else { res = items };
+
+    return res;
+}
